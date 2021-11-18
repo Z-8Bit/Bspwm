2813b10fdf912cff5798bbd575221884ac35800f
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2021-11-18 14:35:08 +0530

    Removed Cava

--- bspwm-big-setup.sh
+++ bspwm-big-setup.sh
@@ -3,7 +3,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S --noconfirm alacritty auto-cpufreq backlight_control bluez bluez-utils bspwm btop cava cmatrix discord dunst figlet firefox-developer-edition fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lollypop lxappearance maim mpv neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseuadio-jack qbittorrent-dark-git qogir-gtk-theme-git refind rofi spotify stacer-git sxhkd teams-for-linux tela-icon-theme thunar touchegg touche ttc-iosevka ttf-material-design-icons ttf-unifont ttf-ms-fonts viewnior vscodium-bin whatsapp-nativefier-dark xclip xorg zoom
+paru -S --noconfirm alacritty auto-cpufreq backlight_control bluez bluez-utils bspwm btop cmatrix discord dunst figlet firefox-developer-edition fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lollypop lxappearance maim mpv neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseuadio-jack qbittorrent-dark-git qogir-gtk-theme-git refind rofi spotify stacer-git sxhkd teams-for-linux tela-icon-theme thunar touchegg touche ttc-iosevka ttf-material-design-icons ttf-unifont ttf-ms-fonts viewnior vscodium-bin whatsapp-nativefier-dark xclip xorg zoom
 
 for service in bluetooth lightdm NetworkManager touchegg.service; do
   sudo systemctl enable --now $service
@@ -15,7 +15,6 @@ mkdir -p ~/.config/{alacritty,bspwm,cava,dunst,polybar,rofi,neofetch,sxhkd}
 
 cp ~/Downloads/Bspwm/alacritty/alacritty.yml ~/.config/alacritty/
 cp ~/Downloads/Bspwm/bspwm/bspwmrc ~/.config/bspwm/
-cp ~/Downloads/Bspwm/cava/config ~/.config/cava/
 cp ~/Downloads/Bspwm/dunst/dunstrc ~/.config/dunst/
 cp ~/Downloads/Bspwm/sxhkd/sxhkdrc ~/.config/sxhkd/
 
