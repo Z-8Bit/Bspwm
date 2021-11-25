b61ac145f74a64c949747d86663bb82f2c8fd058
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2021-11-25 10:40:53 +0530

    Update bspwm-big-setup.sh

--- bspwm-big-setup.sh
+++ bspwm-big-setup.sh
@@ -3,7 +3,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S --noconfirm alacritty auto-cpufreq backlight_control bluez bluez-utils bspwm btop cmatrix discord dunst figlet firefox-developer-edition fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lollypop lxappearance maim mpv neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseuadio-jack qbittorrent-dark-git qogir-gtk-theme-git refind rofi spotify stacer-git sxhkd teams-for-linux tela-icon-theme thunar touchegg touche ttc-iosevka ttf-material-design-icons ttf-unifont ttf-ms-fonts viewnior vscodium-bin whatsapp-nativefier-dark xclip xorg zoom
+paru -S --noconfirm alacritty auto-cpufreq backlight_control bluez bluez-utils bspwm btop cmatrix discord dunst figlet firefox-developer-edition fluent-gtk-theme-git fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lollypop lxappearance maim mpv neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseuadio-jack qbittorrent-dark-git qogir-gtk-theme-git refind rofi spotify stacer-git sxhkd teams-for-linux tela-icon-theme thunar touchegg touche ttc-iosevka ttf-material-design-icons ttf-unifont ttf-ms-fonts viewnior vscodium-bin whatsapp-nativefier-dark xclip xorg zoom
 
 for service in bluetooth lightdm NetworkManager touchegg.service; do
   sudo systemctl enable --now $service
@@ -34,6 +34,9 @@ git clone https://github.com/Z-8Bit/Wallpapers ~/Pictures/Wallpapers
 sleep 5
 RED='\033[0;31m'
 echo -e "${RED}Please add ~/Pictures/Wallpapers in nitrogen's preferences"
+
+sudo cp ~/Downloads/Bspwm/lightdm-gtk-greeter.conf /etc/lightdm/
+sudo cp ~/Pictures/Wallpapers/ARTWORK-wanderer-above-the-sea-of-fog.jpg /etc/lightdm/
 
 sleep 5
 echo -e  "${RED}PLEASE REBOOT NOW"
