09b8e58f73eba229d26208ea78738ddfd802c182
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2021-11-25 10:41:34 +0530

    Update bspwm-base-setup.sh

--- bspwm-base-setup.sh
+++ bspwm-base-setup.sh
@@ -3,7 +3,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S --noconfirm alacritty backlight_control bspwm cmatrix dunst figlet fluent-cursor-theme-git lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lxappearance maim neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth qogir-gtk-theme-git rofi sxhkd tela-icon-theme touchegg touche thunar ttf-ms-fonts ttc-iosevka xclip xorg 
+paru -S --noconfirm alacritty backlight_control bspwm cmatrix dunst figlet fluent-gtk-theme-git fluent-cursor-theme-git lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lxappearance maim neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji pavucontrol picom-jonaburg-git polybar pulseaudio pulseaudio-alsa pulseaudio-bluetooth qogir-gtk-theme-git rofi sxhkd tela-icon-theme touchegg touche thunar ttf-ms-fonts ttc-iosevka xclip xorg 
 
 for service in bluetooth lightdm NetworkManager touchegg.service; do
   sudo systemctl enable --now $service
